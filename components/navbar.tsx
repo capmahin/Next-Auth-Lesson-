94ee7c515ea7af23a479a8c1e0a125c357a51719
capmahin <mahinulhuque1996@gmail.com>
2025-04-12 01:41:46 +0600

    import session in layout and navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 
-export default async function Navbar() {
+export default function Navbar() {
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <nav className="flex justify-between items-center py-3 px-4 bg-white shadow-md">
       <Link href="/" className="text-xl font-bold">
         Auth.js
       </Link>
 
-      <Link href="/auth/signin">
-        <Button variant="default">Sign In</Button>
-      </Link>
-
-      <form>
-        <Button variant="default" type="submit">
-          Sign Out
-        </Button>
-      </form>
+      {!session ? (
+        <Link href="/auth/signin">
+          <Button variant="default">Sign In</Button>
+        </Link>
+      ) : (
+        <form>
+          <Button variant="default" type="submit">
+            Sign Out
+          </Button>
+        </form>
+      )}
     </nav>
   );
 }
