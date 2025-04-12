ecee517fb39318ca229e3316a013b20cfac16af8
capmahin <mahinulhuque1996@gmail.com>
2025-04-13 03:44:29 +0600

    add try catch in try add

--- app/actions/authActions.ts
+++ app/actions/authActions.ts
@@ -3,4 +3,23 @@
 import { signIn, signOut } from "@/auth";
 import { AuthError } from "next-auth";
 
-export async function handleCredentialsSignin() {}
+export async function handleCredentialsSignin({
+  email,
+  password
+}: {
+  email: string;
+  password: string;
+}) {
+  try {
+    await signIn("credentials", { email, password, redirectTo: "/" });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            message: "Invalid credentials"
+          };
+      }
+    }
+  }
+}
